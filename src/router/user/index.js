eda7e5a8e46a2a38b4c408260e007670ed87bbe2
yeehui.poh <yhpoh91@gmail.com>
2021-09-20 01:17:49 +0800

    add authentication to all routes

--- src/router/user/index.js
+++ src/router/user/index.js
@@ -3,48 +3,58 @@ const validate = require('express-validation');
 const validator = require('./validation');
 const controller = require('./controller');
 
+const authenticator = require('../../services/authenticator');
+
 const router = express.Router({ mergeParams: true });
 
 router.route('/')
   .get(
+    authenticator.authenticate,
     validate(validator.listUsers),
     controller.listUsers,
   )
   .post(
+    authenticator.authenticate,
     validate(validator.createUser),
     controller.createUser,
   );
 
 router.route('/search')
   .post(
+    authenticator.authenticate,
     validate(validator.searchUsers),
     controller.searchUsers,
   );
 
 router.route('/authenticate')
   .post(
+    authenticator.authenticate,
     validate(validator.authenticateUser),
     controller.authenticateUser,
   );
 
 router.route('/:userId/username')
   .put(
+    authenticator.authenticate,
     validate(validator.updateUsername),
     controller.updateUsername,
   );
 
 router.route('/:userId/password')
   .put(
+    authenticator.authenticate,
     validate(validator.updatePassword),
     controller.updatePassword,
   );
 
 router.route('/:userId')
   .get(
+    authenticator.authenticate,
     validate(validator.getUser),
     controller.getUser,
   )
   .delete(
+    authenticator.authenticate,
     validate(validator.deleteUser),
     controller.deleteUser,
   );
